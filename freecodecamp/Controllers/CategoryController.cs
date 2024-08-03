0d9aae90e301c76f547cd44e08c6c1f4d066ce1f
Ashish karki <134418831+IamAshish2@users.noreply.github.com>
2024-08-04 01:27:21 +0545

    Add project files.

---
+++ freecodecamp/Controllers/CategoryController.cs
@@ -0,0 +1,112 @@
+﻿using freecodecamp.context;
+using freecodecamp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace freecodecamp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db) {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if ((obj.Name == obj.DisplayOrder.ToString()))
+            {
+                ModelState.AddModelError("Name", "The display order can't be same as the category name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null) {
+                return NotFound();
+
+            }
+            return View(categoryFromDb); 
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if ((obj.Name == obj.DisplayOrder.ToString()))
+            {
+                ModelState.AddModelError("Name", "The display order can't be same as the category name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Delete
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (obj == null) {
+                return NotFound();
+                }
+
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            return Ok(obj);
+        }
+    }
+}
